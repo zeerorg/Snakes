28489246f5d635217992ee60db6a4302d21f22bb
Rishabh Gupta <r.g.gupta@outlook.com>
2016-02-09 10:45:29 +0530

    Game v0.1.1

--- snakes.py
+++ snakes.py
@@ -8,12 +8,22 @@ white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
 
+'''Font'''
+font = pygame.font.SysFont(None, 25)    
+
 WIDTH = 800
 HEIGHT = 600
 FPS = 10
 gamedisplay = pygame.display.set_mode([WIDTH,HEIGHT])
 pygame.display.set_caption('Snakes')
 gamedisplay.fill(white)
+
+def message(text, color):
+    screen_text = font.render(text, True, color)
+    gamedisplay.blit(screen_text,
+                     [(WIDTH-(8*len(text)))/2,
+                      (HEIGHT-10)/2])
+    pygame.display.update()
 
 def check_area(pos, lvl=1):
     if lvl == 1:
@@ -29,7 +39,8 @@ snake = [ [WIDTH/2+20, HEIGHT/2, 10, 10],
 
 pyClock = pygame.time.Clock()
 gameExit = False
-food = [random.randint(10, (WIDTH/10-10))*10, random.randint(10, (HEIGHT/10-10))*10, 10, 10]
+food = [random.randint(10, (WIDTH/10-10))*10,
+        random.randint(10, (HEIGHT/10-10))*10, 10, 10]
 
 speed = 10
 vel_x = speed
@@ -92,11 +103,13 @@ while not gameExit:
         snake.append([food[0]-(snake[-2][0]-snake[-1][0]),
                       food[1]-(snake[-2][1]-snake[-1][1]),
                       10, 10])
-        food = [random.randint(10, WIDTH/10)*10, random.randint(10, HEIGHT/10)*10, 10, 10]
+        food = [random.randint(10, WIDTH/10)*10,
+                random.randint(10, HEIGHT/10)*10, 10, 10]
 
     pygame.display.update()
+    if not gameExit:
+        gameExit = not check_area(snake[0])
     pyClock.tick(FPS)
-    gameExit = not check_area(snake[0])
 
 pygame.quit()
 quit()
